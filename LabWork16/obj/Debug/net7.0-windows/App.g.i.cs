8e0962c2bfca63e5806ed557743a8e7ebfb07f42
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2023-11-03 15:29:16 +0300

    1-4 passed, 5-8 todo

--- LabWork16/obj/Debug/net7.0-windows/App.g.i.cs
+++ LabWork16/obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace LabWork16 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace LabWork16 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public static void Main() {
             LabWork16.App app = new LabWork16.App();
             app.InitializeComponent();
